b17e1d174d7204ec5c795250503e6de18c800771
Lucas Felipe Ritzke <lritzke@furb.br>
2025-08-06 21:46:12 -0300

    primeira aula

---
+++ Furb/aulas/PO/test/src/test/Cachorro.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Cachorro {
+
+	public String nome;
+	public int idade;
+	
+	
+	public void latir() {
+		System.out.println("Nome: " + nome + " Idade: " + idade);
+		System.out.println("auau");
+	}
+
+}
---
+++ Furb/aulas/PO/test/src/test/main.java
@@ -0,0 +1,12 @@
+package test;
+
+public class main {
+	public static void main(String[] args){
+		Cachorro c;
+		c = new Cachorro();
+		c.nome = "Biscoito";
+		c.idade = 1;
+		
+		c.latir();
+	}
+}
